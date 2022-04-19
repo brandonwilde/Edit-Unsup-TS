bb22117ce2b6f722c5fa70ac3a30a48b3c56ec69
Brandon Wilde <74289641+brandonwilde@users.noreply.github.com>
2022-04-19 16:16:05 -0400

    Sped up sampling, added class lit examples

---
+++ src/connect_to_corenlp.cmd
@@ -0,0 +1,3 @@
+cd \Users\brand\anaconda3\envs\UnsupTS\Lib\stanford-corenlp-4.4.0
+java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer -port 9000 -timeout 15000
+pause
--- src/main.py
+++ src/main.py
@@ -28,11 +28,31 @@ if config['operation'] == 'train_lm':
 	trainIters(decoder, train_simple_unique, valid_simple_unique, test_simple_unique, output_lang, tag_lang, dep_lang, train_pos, train_dep, valid_pos, valid_dep, test_pos, test_dep)
 
 
+# elif config['operation'] == "sample":
+# 	from model.structural_decoder import DecoderGRU
+
+# 	idf, unigram_prob, output_lang, tag_lang, dep_lang, train_simple, valid_simple, test_simple, train_complex, valid_complex, test_complex, output_embedding_weights, tag_embedding_weights, dep_embedding_weights = prepareData(config['embedding_dim'], 
+# 	config['freq'], config['ver'], config['dataset'], config['operation'])
+
+# 	lm_forward = DecoderGRU(config['hidden_size'], output_lang.n_words, tag_lang.n_words, dep_lang.n_words, config['num_layers'], 
+# 		output_embedding_weights, tag_embedding_weights, dep_embedding_weights, config['embedding_dim'], config['tag_dim'], config['dep_dim'], config['dropout'], config['use_structural_as_standard']).to(device)
+# 	lm_backward = DecoderGRU(config['hidden_size'], output_lang.n_words, tag_lang.n_words, dep_lang.n_words, config['num_layers'], 
+# 		output_embedding_weights, tag_embedding_weights, dep_embedding_weights, config['embedding_dim'], config['tag_dim'], config['dep_dim'], config['dropout'], config['use_structural_as_standard']).to(device)
+
+# 	from tree_edits_beam import *
+# 	if config['set'] == 'valid':
+# 		sample(valid_complex, valid_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
+# 	elif config['set'] == 'test':
+# 		sample(test_complex, test_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
+
+
 elif config['operation'] == "sample":
 	from model.structural_decoder import DecoderGRU
+        
+	idf, unigram_prob, output_lang, tag_lang, dep_lang, split_simple, split_complex, output_embedding_weights, tag_embedding_weights, dep_embedding_weights = prepareDataSingle(config['embedding_dim'], config['freq'], config['ver'], config['dataset'], config['operation'], config['set'])
 
-	idf, unigram_prob, output_lang, tag_lang, dep_lang, train_simple, valid_simple, test_simple, train_complex, valid_complex, test_complex, output_embedding_weights, tag_embedding_weights, dep_embedding_weights = prepareData(config['embedding_dim'], 
-	config['freq'], config['ver'], config['dataset'], config['operation'])
+# 	idf, unigram_prob, output_lang, tag_lang, dep_lang, train_simple, valid_simple, test_simple, train_complex, valid_complex, test_complex, output_embedding_weights, tag_embedding_weights, dep_embedding_weights = prepareData(config['embedding_dim'], 
+# 	config['freq'], config['ver'], config['dataset'], config['operation'])
 
 	lm_forward = DecoderGRU(config['hidden_size'], output_lang.n_words, tag_lang.n_words, dep_lang.n_words, config['num_layers'], 
 		output_embedding_weights, tag_embedding_weights, dep_embedding_weights, config['embedding_dim'], config['tag_dim'], config['dep_dim'], config['dropout'], config['use_structural_as_standard']).to(device)
@@ -40,10 +60,12 @@ elif config['operation'] == "sample":
 		output_embedding_weights, tag_embedding_weights, dep_embedding_weights, config['embedding_dim'], config['tag_dim'], config['dep_dim'], config['dropout'], config['use_structural_as_standard']).to(device)
 
 	from tree_edits_beam import *
-	if config['set'] == 'valid':
-		sample(valid_complex, valid_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
-	elif config['set'] == 'test':
-		sample(test_complex, test_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
+	sample(split_complex, split_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
+
+# 	if config['set'] == 'valid':
+# 		sample(valid_complex, valid_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
+# 	elif config['set'] == 'test':
+# 		sample(test_complex, test_simple, output_lang, tag_lang, dep_lang, lm_forward, lm_backward, output_embedding_weights, idf, unigram_prob)
 
 else:
 	print('incorrect operation')
